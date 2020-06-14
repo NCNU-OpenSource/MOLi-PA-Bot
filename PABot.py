615ac57fb28720f1895df2859bedfd09fae815c6
vincentinttsh <me@vincentinttsh.tw>
2020-06-15 02:21:02 +0800

    connect problem fixed

--- PABot.py
+++ PABot.py
@@ -215,15 +215,12 @@ def play(msg) :
     video = pafy.new(url)
     best = video.getbest()
     playurl = best.url
-    Instance = vlc.Instance()
-    player = Instance.media_player_new()
-    Media = Instance.media_new(playurl)
-    Media.get_mrl()
-    player.set_media(Media)
+    vlc.Instance("prefer-insecure")
+    player = vlc.MediaPlayer(playurl)
     volume = 60
     player.audio_set_volume(volume)
-    player.play()
-    time.sleep(1.5)
+    a = player.play()
+    time.sleep(5)
     playStatus = 'play'
     # duration = player.get_length() / 1000
     # time.sleep(duration)
